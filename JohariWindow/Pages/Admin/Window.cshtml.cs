91e0ab4a5603917fa37874d2fe72b53393e1a49e
ryanb <ryanb@LAPTOP-KQMFTSDA>
2021-10-12 09:46:23 -0600

    Window changes

--- JohariWindow/Pages/Admin/Window.cshtml.cs
+++ JohariWindow/Pages/Admin/Window.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApplicationCore.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,14 @@ namespace JohariWindow.Pages.Admin
 {
     public class WindowModel : PageModel
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public ApplicationCore.Models.Client Client;
+        public WindowModel(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+
         public void OnGet(int ClientID)
         {
+            Client = _unitOfWork.Client.Get(i => i.ClientID == ClientID);
         }
     }
 }
